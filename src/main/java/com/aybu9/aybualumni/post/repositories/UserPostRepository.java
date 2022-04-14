1159cb47f5993af8d8bcb184b5a8bc6443ffc776
ZootHii <ahmet-yildirim_5858@outlook.com>
2022-04-14 18:00:45 +0300

    comment unnecessary dependencies and tests

--- src/main/java/com/aybu9/aybualumni/post/repositories/UserPostRepository.java
+++ src/main/java/com/aybu9/aybualumni/post/repositories/UserPostRepository.java
@@ -1,11 +1,11 @@
 package com.aybu9.aybualumni.post.repositories;
 
 import com.aybu9.aybualumni.post.models.UserPost;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/test/java/com/aybu9/aybualumni/TestUtils.java
+++ src/test/java/com/aybu9/aybualumni/TestUtils.java
@@ -1,29 +1,29 @@
-package com.aybu9.aybualumni;
-
-import com.aybu9.aybualumni.user.models.User;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-
-public class TestUtils {
-
-    public static List<User> generateUsers() {
-        return IntStream.range(0, 5).mapToObj(value -> User.builder()
-                .id((long) value)
-                .email(value + "@test.com")
-                .password(value + "password")
-                .name(value + "name")
-                .surname(value + "surname")
-                .nameInCollege(value + "nameInCollege")
-                .surnameInCollege(value + "surnameInCollege")
-                .department(value + "department")
-                .grade(value + "grade")
-                .about(value + "about")
-                .profileUrl(value + "profileUrl")
-                .profileUrl(value + "profileUrl")
-                .profilePhotoUrl(value + "profilePhotoUrl")
-                .coverPhotoUrl(value + "coverPhotoUrl")
-                .build()).collect(Collectors.toList());
-    }
-}
+//package com.aybu9.aybualumni;
+//
+//import com.aybu9.aybualumni.user.models.User;
+//
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.IntStream;
+//
+//public class TestUtils {
+//
+//    public static List<User> generateUsers() {
+//        return IntStream.range(0, 5).mapToObj(value -> User.builder()
+//                .id((long) value)
+//                .email(value + "@test.com")
+//                .password(value + "password")
+//                .name(value + "name")
+//                .surname(value + "surname")
+//                .nameInCollege(value + "nameInCollege")
+//                .surnameInCollege(value + "surnameInCollege")
+//                .department(value + "department")
+//                .grade(value + "grade")
+//                .about(value + "about")
+//                .profileUrl(value + "profileUrl")
+//                .profileUrl(value + "profileUrl")
+//                .profilePhotoUrl(value + "profilePhotoUrl")
+//                .coverPhotoUrl(value + "coverPhotoUrl")
+//                .build()).collect(Collectors.toList());
+//    }
+//}
