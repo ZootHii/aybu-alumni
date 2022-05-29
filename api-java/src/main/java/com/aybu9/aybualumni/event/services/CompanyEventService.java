2741270e2436c49529c93beac0522b55e54c12dd
emrhnrl <emrhnrl@hotmail.com>
2022-05-29 16:13:56 +0300

    companyevent pageable

--- api-java/src/main/java/com/aybu9/aybualumni/event/services/CompanyEventService.java
+++ api-java/src/main/java/com/aybu9/aybualumni/event/services/CompanyEventService.java
@@ -3,6 +3,7 @@ package com.aybu9.aybualumni.event.services;
 import com.aybu9.aybualumni.core.result.DataResult;
 import com.aybu9.aybualumni.event.models.CompanyEvent;
 import com.aybu9.aybualumni.event.models.dtos.CompanyEventDto;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,6 +12,8 @@ import java.util.Collection;
 public interface CompanyEventService {
 
     DataResult<Collection<CompanyEvent>> getAll();
+
+    DataResult<Collection<CompanyEvent>> getAllPageable(Pageable pageable);
 
     DataResult<CompanyEvent> create(Authentication authentication, Long userId, CompanyEventDto companyEventDto,
                                     MultipartFile multipartFile);
